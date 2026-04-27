752b22a9660ee8ba7d4a0d394b049aa81e3233ce
charms <cord.harms@loproducts.de>
2026-04-27 22:00:24 +0200

    Vorbereitung fuer A2

---
+++ Programs/A2ASM/README.md
@@ -0,0 +1,65 @@
+# Aufgaben
+
+## Code-Anweisungen
+
+**Beschreiben Sie in der Datei `Programs/A2/README.md` für jede Zeile den Effekt auf die Register `R0`, `R2` und `R3` und den Speicher (Memory) ab der Adresse der Variable `VariableA`.**
+
+
+### Code-Abschnitt
+```asm
+ldr    R0,=VariableA     ; Anw01
+ldrb   R2,[R0]           ; Anw02
+ldrb   R3,[R0,#1]        ; Anw03
+lsl    R2, #8            ; Anw04
+orr    R2, R3            ; Anw05
+strh   R2,[R0]           ; Anw06
+```
+
+
+### Anweisung 01
+```
+ldr R0,=VariableA ; Anw01
+```
+Die Speicher-Referenz von `VariableA` wird in `R0` gespeichert.
+
+
+### Anweisung 02
+```
+ldrb R2,[R0] ; Anw02
+```
+Der Least-Significant-Byte, des Inhalts, welcher an der Speicheradresse `R0`  steht, wird in `R2` gespeichert (`0xef`).
+
+
+### Anweisung 03
+```
+ldrb R3,[R0,#1] ; Anw03
+```
+Der zweite Byte, des Inhalts, welcher an der Speicheradresse `R0`  steht, wird in `R2` gespeichert (`0xbe`). `#1` gibt die Anzahl der Verschiebungen um die Größe der Lade-Operation an.
+
+
+### Anweisung 04
+```
+lsl R2, #8 ; Anw04
+```
+Eine Left-Shift-Operation wird durchgeführt. Es werden alle Bits um `#8` Stellen nach links verschoben. Liegt das Zielbit außerhalb der Speichergröße, beginnt die Verschiebung wieder beim least significant bit. Das Ergebnis bleibt in `R2`.
+Es gilt also: Zielposition = (Position + Verschiebung) mod Speichergröße
+`0x 0000 0000 0000 00ef` $\rightarrow$ `0x 0000 0000 0000 ef00`
+
+
+### Anweisung 05
+```asm
+orr R2, R3 ; Anw05
+```
+Eine `XOR` Operation zwischen den beiden angegebenen Registern `R2` und `R3`, von welcher das Ergebnis in `R2` gespeichert wird.
+```
+0xEF00            = 0b1110 1111 0000 0000
+0x00BE            = 0b0000 0000 1011 1110
+0xEF00 XOR 0x00BE = 0b1110 1111 1011 1110 = 0xEFBE
+```
+
+
+### Anweisung 06
+```
+strh R2,[R0] ; Anw06
+```
+Speichert das Least-Significant Halbwort in `R2`, an der Speicheradresse, welche in `R0` angegeben ist.
---
+++ Programs/A2ASM/Src/main.s
@@ -0,0 +1,55 @@
+;******************** (C) COPYRIGHT HAW-Hamburg ********************************
+;* File Name          : main.s
+;* Author             : Martin Becke    
+;* Version            : V1.0
+;* Date               : 01.06.2021
+;* Description        : This is a simple main to demonstrate data transfer
+;                     : and manipulation.
+;                     : 
+;
+;*******************************************************************************
+    EXTERN initITSboard ; Helper to organize the setup of the board
+
+    EXPORT main         ; we need this for the linker - In this context it set the entry point,too
+
+ConstByteA  EQU 0xaffe			;0b 1010 1111 1111 1110
+								;ConstByteA == 0xaffe == true
+    
+;* We need some data to work on
+    AREA DATA, DATA, align=2    
+VariableA   DCW 0xbeef			;0b 1011 1110 1110 1111
+VariableB   DCW 0x1234			;0b 0001 0010 0011 0100
+VariableC	DCW 0x0000			;reserve 2 words in memory
+
+;* We need minimal memory setup of InRootSection placed in Code Section 
+    AREA  |.text|, CODE, READONLY, ALIGN = 3    
+    ALIGN   
+main
+    BL initITSboard             ; needed by the board to setup
+;* swap memory - Is there another, at least optimized approach?
+    ldr     R0,=VariableA   ; Anw01
+    ldrb    R2,[R0]         ; Anw02
+    ldrb    R3,[R0,#1]      ; Anw03
+    lsl     R2, #8          ; Anw04
+    orr     R2, R3          ; Anw05
+    strh    R2,[R0]         ; Anw06
+    
+;* const in var
+    mov     R5,#ConstByteA  ; Anw07
+    strh    R5,[R0]         ; Anw08
+
+;* save 0xaffe in memory
+	ldr		R10,=VariableC	; load memory address
+	strh	R5, [R10]		; saves 0xfeaf in memory
+
+
+;* Change value from x1234 to x4321
+    ldr     R1,=VariableB   ; Anw09
+    ldrh    R6,[R1]         ; Anw0A
+    mov     R7, #0x30ED     ; Anw0B
+    add     R6, R6, R7      ; Anw0C
+    strh    R6,[R1]         ; Anw0D
+    b .                     ; Anw0E
+    
+    ALIGN
+    END
